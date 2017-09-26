a8bf37b60ecd66f379046aa810fc1fd3f78445c3
caov <caovan@163.com>
2017-09-26 14:59:28 +0800

    webuploader分片断点的代码

---
+++ webuploader/src/main/java/com/van/WebuploaderApplication.java
@@ -0,0 +1,12 @@
+package com.van;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebuploaderApplication.class, args);
+	}
+}
